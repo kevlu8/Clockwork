17b2197865a000c4d22c579df15e60523459a742
Kevin Lu <69993704+kevlu8@users.noreply.github.com>
2026-01-07 19:47:53 +0000

    Add mate guard
    Bench: 13928703

--- src/search.cpp
+++ src/search.cpp
@@ -487,7 +487,7 @@ Value Worker::search(
     }
 
     if (!PV_NODE && !is_in_check && depth <= tuned::rfp_depth && !excluded
-        && tt_adjusted_eval >= beta + tuned::rfp_margin * depth) {
+        && tt_adjusted_eval >= beta + tuned::rfp_margin * depth && !is_being_mated_score(beta)) {
         return (tt_adjusted_eval + beta) / 2;
     }
 
